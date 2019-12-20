3a24d206c119268c95fc598ba20eb416d72b1f79
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-20 15:38:21 -0300

    Adding a click listener

--- app/src/main/java/com/joabe/aboutme/MainActivity.kt
+++ app/src/main/java/com/joabe/aboutme/MainActivity.kt
@@ -2,11 +2,24 @@ package com.joabe.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
     }
 }
