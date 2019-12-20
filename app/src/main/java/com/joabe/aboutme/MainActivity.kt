17bcaa26f6c77f6bc3b209ac68a83f8979594a1a
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-12-20 15:46:49 -0300

    Adding a click listener to the nickname TextView

--- app/src/main/java/com/joabe/aboutme/MainActivity.kt
+++ app/src/main/java/com/joabe/aboutme/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickName(it)
+        }
     }
 
     private fun addNickname(view: View) {
@@ -31,5 +35,14 @@ class MainActivity : AppCompatActivity() {
 
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 }
